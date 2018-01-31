af694b1309be762773cd49b28a148e95e3f64b8d
Takhir30 <t.baiheryiev@andersenlab.com>
2018-01-31 12:22:45 +0300

    stage 3

---
+++ db.py
@@ -0,0 +1,35 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Session = sessionmaker()
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    login = Column(String(50))
+    password = Column(String(50))
+    email = Column(String(50))
+    phone = Column(String(50))
+
+
+    def __init__(self, login, password, email, phone):
+        self.login = login
+        self.password = password
+        self.email = email
+        self.phone = phone
+
+
+    def __repr__(self):
+        return "<User('%s','%s', '%s', '%s')>" % (self.login, self.password, self.email, self.phone)
+
+
+for instance in session.query(User):
+    if instanse.login != 'vasia':
+        session.add(User('vasia', '1234', '1@mail.ru', 825649))
+    elif instance.login == 'petya':
+        instance.delete()
+    elif len(instanse.password) < 4:
+        instanse.password += '/*-'
+session.commit()
