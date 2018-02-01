00e2e49c57b762d8fafa5c52b89621b1d2b81962
Takhir30 <t.baiheryiev@andersenlab.com>
2018-02-01 11:20:33 +0300

    parse and log

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask
+def main():
+    logging.basicConfig(format = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s[%(asctime)s]  %(message)s', level = logging.DEBUG)
+    logging.info(u'Prog started')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', default = True, choices=[True, False], help = 'Debug status?')
+    parser.add_argument('--host', default = "127.0.0.1", help = 'What host?')
+    parser.add_argument('--port', default = 5000, type = int, help = 'What port?')
+    args = parser.parse_args()
 
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    @app.route('/')
+    def hello():
+        return "Hello, world!"
 
-
-@app.route('/')
-def hello():
-    return "Hello, world!"
-
+    app.run(args)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()
+    logging.info(u'Prog finished')
