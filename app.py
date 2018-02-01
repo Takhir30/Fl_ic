53ce840275e83eec6f09bb27cb427d6dfaf30341
Takhir30 <t.baiheryiev@andersenlab.com>
2018-02-01 12:00:39 +0300

    fixed

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
+from flask import Flask
+import argparse
+import logging
+
+
+@app.route('/')
+def hello():
+    return "Hello, world!"
+
+
 def main():
+    app = Flask(__name__)
+
+
+if __name__ == '__main__':
+    main()
     logging.basicConfig(format = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s[%(asctime)s]  %(message)s', level = logging.DEBUG)
-    logging.info(u'Prog started')
     parser = argparse.ArgumentParser()
     parser.add_argument('--debug', default = True, choices=[True, False], help = 'Debug status?')
     parser.add_argument('--host', default = "127.0.0.1", help = 'What host?')
     parser.add_argument('--port', default = 5000, type = int, help = 'What port?')
     args = parser.parse_args()
-
-    app = Flask(__name__)
-
-    @app.route('/')
-    def hello():
-        return "Hello, world!"
-
+    logging.info(u'Prog started')
     app.run(args)
-
-if __name__ == '__main__':
-    main()
     logging.info(u'Prog finished')
