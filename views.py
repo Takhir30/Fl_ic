77a38922ee2045c6591e08d767ae75c7a2c76165
Takhir30 <t.baiheryiev@andersenlab.com>
2018-02-07 11:14:58 +0300

    added func chain and view

---
+++ views.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from app import chain
+
+def chain():
+    engine = create_engine(set_address('Data_text'))
+    return engine
+
+db = g.engine = chain()
+connection = db.connect()
+result = connection.execute()
+for row in result:
+    print("user:", row)
+connection.close()
