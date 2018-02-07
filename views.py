df7e96980b7eb74aeff818ecc9c1ff716602d81e
Takhir30 <t.baiheryiev@andersenlab.com>
2018-02-07 16:35:15 +0300

    added "select" to the code

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 from sqlalchemy import MetaData, Table
+from sqlalchemy.sql import select
 from flask import g
 from app import  app, chain
 
@@ -6,6 +7,9 @@ from app import  app, chain
 with app.app_context():
     db = g.engine = chain()
     meta = MetaData(bind=db)
+    conn = db.connect()
     users_table = Table('users', meta, autoload=True)
-    for row in users_table:
+    s = select([users])
+    result = conn.execute(s)
+    for row in result:
         print("user:", row)
